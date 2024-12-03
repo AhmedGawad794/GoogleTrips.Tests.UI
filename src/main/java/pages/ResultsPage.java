7d12941c76ca16cbfd873fedf0347506d4f877a8
Ahmed Gawad <a.gawad@itisglobal.com>
2024-12-03 22:16:55 +0200

    minor refactor

--- src/main/java/pages/ResultsPage.java
+++ src/main/java/pages/ResultsPage.java
@@ -74,36 +74,30 @@ public class ResultsPage {
     public String getDateTime() {
         return elementFinder.getText(dateTime);
     }
-    // Method to check if the Places section is displayed
+    // Restaurant methods
     public boolean isPlacesSectionDisplayed() {
         return elementFinder.findElement(placesSection).isDisplayed();
     }
 
-    // Method to retrieve the list of restaurants on the main page
     public List<WebElement> getRestaurantsList() {
         return elementFinder.findElements(restaurantsList);
     }
 
-    // Click on "More Places" button
     public void clickMorePlaces() {
         elementFinder.click(morePlacesButton);
     }
 
-    // Wait until the restaurants list section is updated on the new page
     public void waitForRestaurantsListUpdate() {
         elementFinder.waitUntilVisible(restaurantsListSection);
     }
 
-    // Select the first rating option from the dropdown
     public void filterByRating() {
         elementFinder.click(ratingDropDown);
         elementFinder.click(firstRatingOption);
     }
 
-    // Retrieve the titles of all restaurants in the list
     public List<String> getRestaurantTitles() {
         elementFinder.waitUntilVisible(restaurantsListSection);
         return elementFinder.getTexts(restaurantTitles);
     }
-
 }
